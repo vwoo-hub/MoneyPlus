537b81561ea9b25475f59ccbc625641c15ca9eb0
Vincent Woo <vincentxwoo@gmail.com>
2025-02-19 23:55:59 -0800

    add test_whenAddingExepnse_thenShowNewTransaction

--- MoneyPlusUITests/Tests/PaymentTest.swift
+++ MoneyPlusUITests/Tests/PaymentTest.swift
@@ -7,7 +7,7 @@ class PaymentTest: BaseTest {
     lazy var paymentPage = PaymentPage(app: app)
     
     func test_whenAddingIncome_thenShowNewTransaction() {
-        let title = "Food from Taco Bell"
+        let title = "Donations"
         let amount = "100.0"
         let date = "15/01/2025"
         
@@ -24,5 +24,24 @@ class PaymentTest: BaseTest {
         
         homePage
             .viewActivityLabels(title, "15 January 2025", "＋$100.0")
+    }
+    
+    func test_whenAddingExepnse_thenShowNewTransaction() {
+        let title = "Food from Taco Bell"
+        let amount = "10.50"
+        let date = "16/01/2025"
+        
+        homePage
+            .deleteAllActivityCells()
+            .tapAddButton()
+        
+        paymentPage
+            .typeTitleTextField(title)
+            .typeAmountTextField(amount)
+            .typeDateTextField(date)
+            .tapSaveButton()
+        
+        homePage
+            .viewActivityLabels(title, "16 January 2025", "ー$10.5")
     }
 }
